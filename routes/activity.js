b42a055f363c58e03fc31f06debe57da2f1d1eb3
Tabreze <90129100+Tabreze@users.noreply.github.com>
2021-09-30 14:48:06 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -62,7 +62,7 @@ exports.edit = function (req, res) {
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
     logData(req);
-    res.send(200, 'Edit');
+    res.status(200).Edit({"success":true});
 };
 
 /*
@@ -80,7 +80,7 @@ exports.save = function (req, res) {
     // Data from the req and put it in an array accessible to the main app.
     console.log( req.body );
     logData(req);
-    res.send(200, 'Save');
+    res.status(200).Save({"success":true});
 };
 
 /*
@@ -118,7 +118,7 @@ exports.execute = function (req, res) {
 
     // FOR TESTING
     logData(req);
-    res.send(200, 'Publish');
+    res.status(200).Pulish({"success":true});
 
     // Used to decode JWT
     // JWT(req.body, process.env.jwtSecret, (err, decoded) => {
@@ -135,7 +135,7 @@ exports.execute = function (req, res) {
     //         var decodedArgs = decoded.inArguments[0];
             
     //         logData(req);
-    //         res.send(200, 'Execute');
+    //         res.status(200).Execute({"success":true});
     //     } else {
     //         console.error('inArguments invalid.');
     //         return res.status(400).end();
@@ -159,7 +159,7 @@ exports.publish = function (req, res) {
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
 //     logData(req);
-//     res.send(200, 'Publish');
+//     res.status(200).Publish({"success":true});
 };
 
 /*
